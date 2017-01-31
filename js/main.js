6578d1b6d52498c226a59fc20b0fc0493de4de3e
Basien Lhuaire <bastien.lhuaire@outlook.fr>
2017-01-31 11:23:12 +0100

    création class pilote

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 
-var camera = new THREE.OrthographicCamera(  window.innerWidth/-2,window.innerWidth/2,window.innerHeight/2,window.innerHeight/-2, 1, 1000 );
-//var camera = new THREE.PerspectiveCamera( 80, window.innerWidth/window.innerheight, 1, 1000 );
+let camera = new THREE.OrthographicCamera(  window.innerWidth/-2,window.innerWidth/2,window.innerHeight/2,window.innerHeight/-2, 1, 1000 );
+//let camera = new THREE.PerspectiveCamera( 80, window.innerWidth/window.innerheight, 1, 1000 );
 camera.position.z = 100;
 scene.add(camera);
 
-var light = new THREE.PointLight( 0xffffff, 1, 1500 );
+let light = new THREE.PointLight( 0xffffff, 1, 1500 );
 light.position.set( 50, 50, 50 );
 scene.add( light );
-var geometry = new THREE.BoxGeometry( 120, 120, 120 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var player = new THREE.Mesh( geometry, material );
-scene.add( player );
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
